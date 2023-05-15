6c851c0125329840da0193af46cd55c9f9e32da6
Geun-Oh <kandy1002@naver.com>
2023-05-15 21:22:47 +0900

    ✅ [test] 전역 스타일 및 theme 적용 테스트

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import './App.css';
 
+import { styled, ThemeProvider } from 'styled-components';
+
+import GlobalStyle from './styles/globalstyle';
+import { theme } from './styles/theme';
 function App() {
   return (
-    <>
-      <div>hello</div>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <St.Div>hello</St.Div>
       <p>new change</p>
-    </>
+    </ThemeProvider>
   );
 }
 
 export default App;
+
+const St = {
+  Div: styled.div`
+    ${({ theme }) => theme.Text.Header.title_bold_32};
+  `,
+};
