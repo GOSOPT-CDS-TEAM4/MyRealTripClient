5786a7880ae1e4b484793410072e584b33c9b416
Geun-Oh <kandy1002@naver.com>
2023-05-16 20:23:56 +0900

    🔧 [config] react/prop-types : off 추가

---
+++ src/components/layout/atom/Text.jsx
@@ -0,0 +1,14 @@
+import { styled } from 'styled-components';
+
+import { theme } from '../../../styles/theme';
+
+const Text = ({ type, innerText }) => <St.Text style={theme.Text[type]}>{innerText}</St.Text>;
+
+export default Text;
+
+const St = {
+  Text: styled.span`
+    position: absolute;
+    font-family: 'SF Pro';
+  `,
+};
--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,21 +1,15 @@
 export const theme = {
   Text: {
-    Header: {
-      title_bold_32: 'font-weight: 700; font-size: 32px; line-height: 48px;',
-      title_bold_24: 'font-weight: 700; font-size: 24px; line-height: 36px;',
-      title_medium_20: 'font-weight: 510; font-size: 20px; line-height: 28px; letter-spacing: -5%;',
-      title_bold_18: 'font-weight: 700; font-size: 18px; line-height: 27px; letter-spacing: -5%;',
-    },
-    Body: {
-      body_bold_16: 'font-weight: 700; font-size: 16px; line-height: 24px; letter-spacing: -5%;',
-      body_bold_14: 'font-weight: 700; font-size: 14px; line-height: 21px; letter-spacing: -5%;',
-      body_medium_14: 'font-weight: 510; font-size: 14px; line-height: 21px; letter-spacing: -5%;',
-    },
-    Detail: {
-      body_regular_14: 'font-weight: 400; font-size: 14px; line-height: 21px; letter-spacing: -5%;',
-      detail_medium_12: 'font-weight: 700; font-size: 12px; line-height: 18px; letter-spacing: -5%;',
-      detail_regular_12: 'font-weight: 400; font-size: 12px; line-height: 18px; letter-spacing: -5%;',
-    },
+    title_bold_32: 'font-weight: 700; font-size: 32px; line-height: 48px;',
+    title_bold_24: 'font-weight: 700; font-size: 24px; line-height: 36px;',
+    title_medium_20: 'font-weight: 510; font-size: 20px; line-height: 28px; letter-spacing: -5%;',
+    title_bold_18: 'font-weight: 700; font-size: 18px; line-height: 27px; letter-spacing: -5%;',
+    body_bold_16: 'font-weight: 700; font-size: 16px; line-height: 24px; letter-spacing: -5%;',
+    body_bold_14: 'font-weight: 700; font-size: 14px; line-height: 21px; letter-spacing: -5%;',
+    body_medium_14: 'font-weight: 510; font-size: 14px; line-height: 21px; letter-spacing: -5%;',
+    body_regular_14: 'font-weight: 400; font-size: 14px; line-height: 21px; letter-spacing: -5%;',
+    detail_medium_12: 'font-weight: 700; font-size: 12px; line-height: 18px; letter-spacing: -5%;',
+    detail_regular_12: 'font-weight: 400; font-size: 12px; line-height: 18px; letter-spacing: -5%;',
   },
   Color: {
     blue1: 'background: #2B96ED;',
