39ca4a4683cc4f9494cd216fc8e3e0c673f8ad60
Eunbeann <ds5bean23@gmail.com>
2023-05-18 01:05:41 +0900

    💄 [style] icon 추가

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import Flex from './components/layout/atom/Flex';
 import DetailTour from './pages/DetailTour';
 
 import LandingPage from './pages/Landing';
+// import Flex from './components/layout/atom/Flex';
+// import Icon from './components/layout/atom/Icon';
+// import Img from './components/layout/atom/Img';
+import TourListPage from './pages/tourListPage';
 import GlobalStyle from './styles/globalstyle';
 import { theme } from './styles/theme';
 
@@ -13,11 +17,17 @@ function App() {
       <GlobalStyle />
 
 
+
       <Flex column justifycontent="space-evenly" alignitems="center">
 
         <DetailTour />
       </Flex>
       <LandingPage />
+      <St.Div>hello</St.Div>
+      <Flex column justifyContent="space-evenly" alignItems="center">
+        <Icon type="search" />
+        <Img type="img_bangkok" width="100px" height="100px" />
+      </Flex>
     </ThemeProvider>
   );
 }
