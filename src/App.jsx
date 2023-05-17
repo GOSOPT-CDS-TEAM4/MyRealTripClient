3173bfe0491b86b43661b9b39464c81e0b12be8e
Geun-Oh <kandy1002@naver.com>
2023-05-17 18:18:54 +0900

    ✨ [feat] LandingPage 작업...

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
 import { ThemeProvider } from 'styled-components';
 
+<<<<<<< HEAD
 import Flex from './components/layout/atom/Flex';
 import DetailTour from './pages/DetailTour';
+=======
+import LandingPage from './pages/Landing';
+>>>>>>> 52ec659 (✨ [feat] LandingPage 작업...)
 import GlobalStyle from './styles/globalstyle';
 import { theme } from './styles/theme';
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
+<<<<<<< HEAD
       <Flex column justifyContent="space-evenly" alignItems="center">
         <DetailTour />
       </Flex>
+=======
+      <LandingPage />
+>>>>>>> 52ec659 (✨ [feat] LandingPage 작업...)
     </ThemeProvider>
   );
 }
