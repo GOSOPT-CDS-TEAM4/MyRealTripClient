875e3679dc28d32076e8faa282fae9063e3a95dc
Geun-Oh <kandy1002@naver.com>
2023-05-18 16:02:22 +0900

    🚨 [lint] stylelint 설정 추가 및 자동정렬

--- src/components/layout/atom/Flex.jsx
+++ src/components/layout/atom/Flex.jsx
@@ -12,10 +12,10 @@ export default Flex;
 
 const St = {
   FlexWrapper: styled.div`
-    width: 100%;
     display: flex;
     flex-direction: ${({ column }) => (column ? 'column' : 'row')};
-    justify-content: ${({ justifycontent }) => justifycontent || 'flex-start'};
     align-items: ${({ alignitems }) => alignitems || 'flex-start'};
+    justify-content: ${({ justifycontent }) => justifycontent || 'flex-start'};
+    width: 100%;
   `,
 };
