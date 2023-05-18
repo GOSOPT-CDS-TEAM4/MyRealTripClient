2e5aef42cec42fe9280ba67de8f6e6c739d906e4
HAE2NI <hae0_02ni@ewhain.net>
2023-05-18 10:51:38 +0900

    ✨ [feat] 디테일 투어 상단 이미지 섹션 추가

--- src/components/DetailPage/DetailTourImg/DetailTourImg.jsx
+++ src/components/DetailPage/DetailTourImg/DetailTourImg.jsx
@@ -1,5 +1,13 @@
+/* eslint-disable jsx-a11y/alt-text */
+import Flex from '../../layout/atom/Flex';
+
+//제목 상단의 이미지
 function DetailTourImg() {
-  return <div>DetailTourImg</div>;
+  return (
+    <Flex>
+      <img src="https://t1.daumcdn.net/cfile/tistory/2157723656165D1D15" width="60%" height="60%" />
+    </Flex>
+  );
 }
 
 export default DetailTourImg;
