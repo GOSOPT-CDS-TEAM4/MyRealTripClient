c73375b60d0bcb65e09d6105d994323b09923bf3
HAE2NI <hae0_02ni@ewhain.net>
2023-05-18 18:48:21 +0900

    🐛 [fix] camel->snake로 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,22 @@
 import { ThemeProvider } from 'styled-components';
 
-<<<<<<< HEAD
 import Flex from './components/layout/atom/Flex';
 import DetailTour from './pages/DetailTour';
-=======
+
 import LandingPage from './pages/Landing';
->>>>>>> 52ec659 (✨ [feat] LandingPage 작업...)
 import GlobalStyle from './styles/globalstyle';
 import { theme } from './styles/theme';
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-<<<<<<< HEAD
-      <Flex column justifyContent="space-evenly" alignItems="center">
+
+
+      <Flex column justifycontent="space-evenly" alignitems="center">
+
         <DetailTour />
       </Flex>
-=======
       <LandingPage />
->>>>>>> 52ec659 (✨ [feat] LandingPage 작업...)
     </ThemeProvider>
   );
 }
