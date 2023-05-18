b909d9aa8e79927bbd53f9fdfabe6857e3ea320c
HAE2NI <hae0_02ni@ewhain.net>
2023-05-18 10:57:22 +0900

    ✨ [feat] detailTour page App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,18 @@
-import { styled, ThemeProvider } from 'styled-components';
+import { ThemeProvider } from 'styled-components';
 
 import Flex from './components/layout/atom/Flex';
-import Icon from './components/layout/atom/Icon';
-import Img from './components/layout/atom/Img';
+import DetailTour from './pages/DetailTour';
 import GlobalStyle from './styles/globalstyle';
 import { theme } from './styles/theme';
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <St.Div>hello</St.Div>
       <Flex column justifyContent="space-evenly" alignItems="center">
-        <Icon type="search" />
-        <Img type="img_bangkok" width="100px" height="100px" />
+        <DetailTour />
       </Flex>
     </ThemeProvider>
   );
 }
 
 export default App;
-
-const St = {
-  Div: styled.div`
-    ${({ theme }) => theme.Text.title_bold_32};
-  `,
-};
