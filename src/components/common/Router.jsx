737a9ac45ef62d6f1a9f953ac0d861ba1f2039e8
Geun-Oh <kandy1002@naver.com>
2023-05-21 21:25:59 +0900

    ✨ [feat] 라우터 구현

---
+++ src/components/common/Router.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import DetailTour from '../../pages/DetailTour';
+import Landing from '../../pages/Landing';
+import Tour from '../../pages/Tour';
+import TourList from '../../pages/TourListPage';
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/detailTour" element={<DetailTour />} />
+        <Route path="/tour" element={<Tour />} />
+        <Route path="/tourList" element={<TourList />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
