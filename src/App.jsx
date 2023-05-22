74bd972d5f9374485dc36d37662ea19dca3dd61c
seohee0112 <ghdtj7@naver.com>
2023-05-22 16:37:59 +0900

    📝 [Docs] 테스트 작성

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { ThemeProvider } from 'styled-components';
 import Router from './components/common/Router';
 import GlobalStyle from './styles/globalstyle';
 import { theme } from './styles/theme';
-
+//테스트 작성
 function App() {
   return (
     <ThemeProvider theme={theme}>
